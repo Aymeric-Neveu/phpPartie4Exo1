c0bf403c1b5f8306ca273fc3afc21dc89ddaa886
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-20 15:26:59 +0100

    exo1

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    function returnTrue($test)
+    {
+      return $test;
+    }
+    echo returnTrue(true);
+    ?>
+  </body>
+</html>
